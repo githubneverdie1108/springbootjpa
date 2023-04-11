b79b8531872258bb1ece9c81c8599ee1718a2efa
githubneverdie1108 <89768699+githubneverdie1108@users.noreply.github.com>
2023-04-11 19:06:07 +0700

    Update API updateMay

--- src/main/java/com/nhatnvq/controller/MayResourc.java
+++ src/main/java/com/nhatnvq/controller/MayResourc.java
@@ -1,12 +1,16 @@
 package com.nhatnvq.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +45,14 @@ public class MayResourc {
 	@DeleteMapping("/may/{maMay}")
 	public void deletaMay(@PathVariable String maMay) {
 		mayRepo.delete(maMay);
+	}
+	
+	@PutMapping("/may")
+	public ResponseEntity<List<May>> update (@RequestBody List<May> mayList){
+		List<May> updatedMay = new ArrayList<>();
+		for (May may : mayList) {
+			updatedMay.add(mayRepo.save(may));
+		}
+		return ResponseEntity.ok(updatedMay);
 	}
 }
