13654cb4682fa3298a2159c50d4725d109c58ff6
githubneverdie1108 <89768699+githubneverdie1108@users.noreply.github.com>
2023-04-11 18:43:43 +0700

    add thêm

--- src/main/java/com/nhatnvq/repository/MayRepository.java
+++ src/main/java/com/nhatnvq/repository/MayRepository.java
@@ -1,9 +1,15 @@
 package com.nhatnvq.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.nhatnvq.entities.May;
 
 
-public interface MayRepository extends CrudRepository<May, String>{
+public interface MayRepository  extends CrudRepository<May, String>{
+	@Query("SELECT m FROM May m WHERE m.viTri = ?")
+	List<May> getListByViTri(String viTri);
+
 }
--- src/main/java/com/nhatnvq/service/MayService.java
+++ src/main/java/com/nhatnvq/service/MayService.java
@@ -1,5 +1,24 @@
 package com.nhatnvq.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nhatnvq.entities.May;
+import com.nhatnvq.repository.MayRepository;
+
+@Service
 public class MayService {
+	@Autowired
+	private MayRepository mayRepository;
+	
+	List<May> getListByViTri(String viTri){
+		return mayRepository.getListByViTri(viTri);
+	}
+	
+	
+	
+	
 	
 }
